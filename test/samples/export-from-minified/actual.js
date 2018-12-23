5b2a8fc39d59625f4c631f8cb5836b7cf6f3cd29
Rich Harris <richard.a.harris@gmail.com>
2018-12-22 23:14:36 -0500

    handle minified export-from declarations

---
+++ test/samples/export-from-minified/actual.js
@@ -0,0 +1,3 @@
+__shimport__.define('./export-from-minified/input.js', ['./foo.js'], function(__import, __exports, __dep_0){ __exports.foo = __dep_0.foo; /*export{foo}from'./foo.js'*/;
+});
+//# sourceURL=./export-from-minified/input.js
