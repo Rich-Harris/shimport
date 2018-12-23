176a1e658abf875e63ff6f1eb3a89f68fa3a05a2
Rich Harris <richard.a.harris@gmail.com>
2018-12-23 11:38:08 -0500

    handle anonymous class extends - fixes #13 (again)

---
+++ test/samples/export-default-unnamed-class-extends/output.js
@@ -0,0 +1,4 @@
+__shimport__.define('./export-default-unnamed-class-extends/input.js', [], function(__import, __exports){ class __default_export extends bar{}foo=42
+__exports.default = __default_export;
+});
+//# sourceURL=./export-default-unnamed-class-extends/input.js
