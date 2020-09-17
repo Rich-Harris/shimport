2f0b93c14b77e28626067b03c9ad12d4da859e39
Rich Harris <richard.a.harris@gmail.com>
2020-09-17 12:42:18 -0400

    switch to uvu

--- test/test.ts
+++ test/test.ts
@@ -1,19 +1,20 @@
+import { test } from 'uvu';
 import * as fs from 'fs';
 import * as assert from 'assert';
 import * as shimport from '../src/index';
 
-describe('shimport', () => {
-	fs.readdirSync('test/samples').forEach(dir => {
-		if (dir[0] === '.') return;
+fs.readdirSync('test/samples').forEach(dir => {
+	if (dir[0] === '.') return;
 
-		it(dir, () => {
-			const input = fs.readFileSync(`test/samples/${dir}/input.js`, 'utf-8');
-			const actual = shimport.transform(input, `./${dir}/input.js`);
-			fs.writeFileSync(`test/samples/${dir}/actual.js`, actual);
+	test(dir, () => {
+		const input = fs.readFileSync(`test/samples/${dir}/input.js`, 'utf-8');
+		const actual = shimport.transform(input, `./${dir}/input.js`);
+		fs.writeFileSync(`test/samples/${dir}/actual.js`, actual);
 
-			const expected = fs.readFileSync(`test/samples/${dir}/output.js`, 'utf-8');
+		const expected = fs.readFileSync(`test/samples/${dir}/output.js`, 'utf-8');
 
-			assert.equal(actual, expected);
-		});
+		assert.equal(actual, expected);
 	});
 });
+
+test.run();
