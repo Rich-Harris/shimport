2954ed097b066371c11a999ded9f70395ec80dea
Rich Harris <richard.a.harris@gmail.com>
2020-09-18 08:46:22 -0400

    wrangle deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,33 @@
-import typescript from 'rollup-plugin-typescript';
-import replace from 'rollup-plugin-replace';
+import sucrase from '@rollup/plugin-sucrase';
+import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
-export default [
-	{
-		input: 'src/index.ts',
-		output: [
-			{ file: pkg.main, format: 'iife' }
-		],
-		name: '__shimport__',
-		plugins: [
-			replace({ __VERSION__: pkg.version }),
-			typescript({
-				typescript: require('typescript')
-			}),
-			terser(),
-			{
-				transformBundle(code) {
-					return {
-						code: code.replace('alert', '(0,eval)'),
-						map: null
-					};
-				}
+const config = dev => ({
+	input: 'src/index.ts',
+	output: [
+		{
+			file: dev ? 'index.dev.js' : pkg.main,
+			format: 'iife',
+			name: '__shimport__'
+		}
+	],
+	plugins: [
+		replace({ __VERSION__: pkg.version }),
+		sucrase({ transforms: ['typescript'] }),
+		!dev && terser(),
+		{
+			transformBundle(code) {
+				return {
+					code: code.replace('alert', '(0,eval)'),
+					map: null
+				};
 			}
-		]
-	},
+		}
+	]
+})
 
-	{
-		input: 'src/index.ts',
-		output: [
-			{ file: 'index.dev.js', format: 'iife' }
-		],
-		name: '__shimport__',
-		plugins: [
-			replace({ __VERSION__: pkg.version }),
-			typescript({
-				typescript: require('typescript')
-			}),
-			{
-				transformBundle(code) {
-					return {
-						code: code.replace('alert', '(0,eval)'),
-						map: null
-					};
-				}
-			}
-		]
-	}
+export default [
+	config(false),
+	config(true)
 ];
