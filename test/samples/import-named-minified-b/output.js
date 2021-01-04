8ccadcd2f06fcd795241e55307816e5ab30398d9
Rich Harris <richard.a.harris@gmail.com>
2021-01-04 18:21:49 -0500

    hoist imports

--- test/samples/import-named-minified-b/output.js
+++ test/samples/import-named-minified-b/output.js
@@ -1,4 +1,4 @@
-__shimport__.define('./import-named-minified-b/input.js', ['./foo.js', './bar.js'], function(__import, __exports, __dep_0, __dep_1){ var foo = __dep_0.foo; /*import{foo}from './foo.js'*/;var bar = __dep_1.bar; /*import{bar}from './bar.js'*/;
+__shimport__.define('./import-named-minified-b/input.js', ['./foo.js', './bar.js'], function(__import, __exports, __dep_0, __dep_1){ var foo = __dep_0.foo; var bar = __dep_1.bar; /*import{foo}from './foo.js'*/;/*import{bar}from './bar.js'*/;
 
 console.log(foo, bar);
 });
